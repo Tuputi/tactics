2d699886bfee02225cb68b8b06702b6e6eb44cad
Tove <tove.brant@gmail.com>
2015-12-13 19:11:45 +0200

    Editor UI started

---
+++ Tactics/Assets/Scripts/UI/SlidingMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SlidingMenu : MonoBehaviour {
+
+    public GameObject panel;
+    public bool visible;
+
+    
+    public void Show()
+    {
+        if (visible)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        RectTransform r = panel.GetComponent<RectTransform>();
+        panel.transform.localPosition -= new Vector3(0, r.rect.height, 0);
+        visible = true;
+    }
+
+    public void Close()
+    {
+        visible = false;
+        RectTransform r = panel.GetComponent<RectTransform>();
+        panel.transform.localPosition += new Vector3(0, r.rect.height, 0);
+    }
+    
+    
+    
+     
+}
