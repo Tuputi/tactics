11743cbbabd42f69f395c39aadeb2b6461bc3639
Tove <tove.brant@gmail.com>
2016-02-08 13:05:57 +0200

    dial menu

--- Tactics/Assets/Scripts/UI/RotatinMenu.cs
+++ Tactics/Assets/Scripts/UI/RotatinMenu.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class RotatinMenu : MonoBehaviour {
 
     public GameObject RotationPoint;
+    public List<GameObject> ButtonObjects;
     public float speed = 5f;
     public float RotationAmount;
+
+    void Start()
+    {
+        PlaceEvenly();
+    }
 
     void Update()
     {
@@ -25,10 +31,33 @@ public class RotatinMenu : MonoBehaviour {
             RotationPoint.transform.Rotate(new Vector3(0, 0, -RotationAmount), Space.Self);
             for (int i = 0; i < RotationPoint.transform.childCount; i++)
             {
-                RotationPoint.transform.GetChild(i).GetComponent<RotatingMenuSlot>().ChangeRotation(RotationAmount);
+                RotatingMenuSlot tempSlot = RotationPoint.transform.GetChild(i).GetComponent<RotatingMenuSlot>();
+                tempSlot.ChangeRotation(RotationAmount);
             }
             TouchInput.state = TouchInput.TouchState.still;
         }
     }
 
+    public void PlaceEvenly()
+    {
+        float angleBetweenObjects = 360 / ButtonObjects.Count+1;
+        RotationAmount = angleBetweenObjects+3f;
+        float distanceBetweenObjects = 400f;
+        double tempDouble = System.Math.Cos(angleBetweenObjects * 0.5);
+        float tempFloat = (float)tempDouble;
+        float distanceFromCenter = (distanceBetweenObjects * 0.5f) /tempFloat;
+        float accumulatedAngle = 0.0f;
+        //foreach (GameObject go in ButtonObjects )
+        for(int i = 0; i < ButtonObjects.Count-1; i++)
+        {
+            GameObject newG = Instantiate(ButtonObjects[i].gameObject);
+            newG.transform.SetParent(RotationPoint.transform);
+            double cosResult = System.Math.Cos(accumulatedAngle) * distanceFromCenter;
+            double sinResult = System.Math.Sin(accumulatedAngle) * distanceFromCenter;
+            float cosFloat = (float)cosResult;
+            float sinFloat = (float)sinResult;
+            newG.transform.localPosition = new Vector3(cosFloat,sinFloat, 0);
+            accumulatedAngle += angleBetweenObjects;
+        }
+    }
 }
--- Tactics/Assets/Scripts/UI/RotatingMenuSlot.cs
+++ Tactics/Assets/Scripts/UI/RotatingMenuSlot.cs
@@ -5,6 +5,11 @@ public class RotatingMenuSlot : MonoBehaviour {
 
 	public void ChangeRotation(float amount)
     {
-        this.gameObject.transform.Rotate(new Vector3(0, 0, amount), Space.Self);
+        
+        this.gameObject.transform.Rotate(new Vector3(0, 0, amount), Space.Self);    
+    }
+    public void SetOnTop()
+    {
+       this.gameObject.transform.SetAsLastSibling();
     }
 }
